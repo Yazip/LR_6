ef81489254b7a05dbdfea0e2a0eb838a3d7f2586
Ilya Baretkin <usaasu386@mail.ru>
2024-01-16 18:51:17 +0700

    Created class template with Publisher class

--- main.cpp
+++ main.cpp
@@ -8,10 +8,12 @@ int main() {
 	setlocale(0, "");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	Book* book = new Book();
-	Magazine* magazine = new Magazine();
-	book->read();
-	magazine->read();
+	Publisher<string> publisher1;
+	publisher1.setPublisherData("ЭКСМО", "123308, г. Москва, ул. Зорге, д.1, стр.1.", "+7 (495) 411-68-86", "info@eksmo.ru");
+	publisher1.getPublisherData();
+	Publisher<int> publisher2;
+	publisher2.setPublisherData("МИФ", "Москва, м. Пушкинская, Тверская, Чеховская, Большой Козихинский пер., д. 7, стр. 2, 2-й этаж, оф. 24", 591101, "support@m-i-f.ru");
+	publisher2.getPublisherData();
 	_getch();
 	return 0;
 }
